21cc3811a6183bdd0d2ff4edca651684fa826fb8
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-02-26 20:45:47 +0530

    initial commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ weight = input("enter your weight in kg: ")
 weight_as_int = int(weight)
 height_as_float = float(height)
 
-
 bmi = weight_as_int / height_as_float ** 2
 #OR
 bmi = weight_as_int / (height_as_float * height_as_float)
